1276b8d19a7619e67f716a70b224a9beeb26f816
Lucas Trias <lucas.trias@catho.com>
2018-08-29 11:56:18 -0300

    Reading data from DB

--- api/time_event_repository.go
+++ api/time_event_repository.go
@@ -1,16 +1,54 @@
 package api
 
-import "time"
+import (
+	"database/sql"
+	"log"
+
+	_ "github.com/go-sql-driver/mysql"
+)
 
 //TimeEventRepository repository for time events
 type TimeEventRepository struct {
+	db *sql.DB
+}
+
+//NewTimeEventRepository constructs a new instance
+func NewTimeEventRepository() *TimeEventRepository {
+	result := new(TimeEventRepository)
+
+	db, err := sql.Open("mysql", "root:@tcp(localhost:3306)/time_analytics?parseTime=true")
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	result.db = db
+
+	return result
 }
 
 //LoadAllEvents loads all events
 func (t *TimeEventRepository) LoadAllEvents() []TimeEvent {
+	stmt, err := t.db.Prepare("SELECT dia, tipo, quem, tempo_ocupado, tema, departamento, recorrente FROM time_event")
+
+	if err != nil {
+		panic(err.Error())
+	}
+	defer stmt.Close()
+
+	rows, err := stmt.Query()
+	defer rows.Close()
 	var result []TimeEvent
 
-	result = append(result, TimeEvent{time.Now(), "1:1", "Trias", 60, "Programming", "Devel", false})
+	for rows.Next() {
+		r := TimeEvent{}
+		err := rows.Scan(&r.Day, &r.Type, &r.Who, &r.Duration, &r.Subject, &r.Department, &r.Recurrent)
+
+		if err != nil {
+			log.Fatal(err)
+		}
+		result = append(result, r)
+	}
 
 	return result
 }
