f6052745f0435f29f70109c261a0fd26242cbbab
ltrias <lucas.trias@gmail.com>
2018-08-30 13:52:41 -0300

    Adding loadEvent endpoint

--- api/model.go
+++ api/model.go
@@ -6,11 +6,11 @@ import (
 
 //TimeEvent represents a time event such as a meeting
 type TimeEvent struct {
-	ID         uint      `json:"id"`
+	ID         int       `json:"id"`
 	Day        time.Time `json:"day"`
 	Type       string    `json:"type"`
 	Who        string    `json:"who"`
-	Duration   uint      `json:"duration"`
+	Duration   int       `json:"duration"`
 	Subject    string    `json:"subject"`
 	Department string    `json:"department"`
 	Recurrent  bool      `json:"recurrent"`
